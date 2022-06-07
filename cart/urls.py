ee6908f983b5cf392827b165c4bb545546756eb8
spacecat <blablabla@gmail.com>
2022-06-07 21:39:54 +0300

    Cart was fixedgit add .git add . Cheers!

--- cart/urls.py
+++ cart/urls.py
@@ -5,7 +5,7 @@ app_name = 'cart'
 urlpatterns = [
     url(r'^$', views.cart_detail, name='cart_detail'),
     url(r'^add/(?P<item_id>\d+)/$', views.cart_add, name='cart_add'),
-    url(r'^remove/(?P<item_id>\d+)/$', views.cart_remove, name='cart_remove'),
+    url('^remove/(?P<item_id>\d+)?$', views.cart_remove, name='cart_remove'),
     
 
 ]
