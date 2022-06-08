4817406e9edd3434eef857418142d6bd9251e4b4
spacecat <blablabla@gmail.com>
2022-06-08 20:24:23 +0300

    Added orders creation and save

--- flamme/urls.py
+++ flamme/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('main.urls')),
     
    
--- orders/models.py
+++ orders/models.py
@@ -29,7 +29,7 @@ class OrderItem(models.Model):
         product = models.ForeignKey(Item, related_name='order_items', on_delete=models.CASCADE)
         price = models.DecimalField(max_digits=10, decimal_places=2)
         quantity = models.PositiveIntegerField(default=1)
-
+        size = models.BooleanField(default=False)
         def __str__(self):
             return '{}'.format(self.id)
 
