1b1d962f9b86c4a83a0c7c7f62ad328c25f98a5a
spacecat <blablabla@gmail.com>
2022-06-14 21:57:25 +0300

    Added order notifications to bot

--- bot/bot_main.py
+++ bot/bot_main.py
@@ -1,13 +1,47 @@
-import sqlite3
+import sqlite3, time
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
 """DB connect"""
 conn = sqlite3.connect('/home/spacecat/CODE/phoenix_flamme2/phoenix_db3', check_same_thread=False)
 cursor = conn.cursor()
+#id of last executed order
+executed = 0
 
 """DB funcs"""
+
+def timer(update, context):
+    """timer for db upgrade"""
+    while True:
+        limit = 10800
+        time.sleep(limit)
+        db_count(update)   
+
+def db_count(update): #WORKS!
+    """count lines in db table"""
+    cursor.execute("SELECT id FROM orders_order")
+    tmp = cursor.fetchall()
+    for i in tmp:
+        last_order = i[0]
+    db_updater(last_order, update)
+
+def db_updater(last_order, update):# WORKS
+    """check db for new orders and take all new orders"""
+    global executed
+    if last_order > executed:
+        cursor.execute("SELECT id FROM orders_order WHERE ?<id", (executed, ))
+        new_orders = cursor.fetchall()
+        while executed != last_order:
+            for i in new_orders:
+                order_id = i[0]
+                print('last orders',order_id) #test
+                update.message.reply_text('Новый заказ')
+                get_order_info(update, order_id)
+                executed = order_id
+                print('ex', executed) #test
+
 def get_order_info (update,order_id):
+    """search order by id"""
     try:
         
         #execute customer's data
@@ -20,10 +54,9 @@ def get_order_info (update,order_id):
                        FROM main_item INNER JOIN orders_orderitem 
                         ON main_item.id=orders_orderitem.product_id WHERE orders_orderitem.order_id=?""", (order_id,))
         item_data = cursor.fetchall()                 
-        print(cursor.fetchall())
-        update.message.reply_text(f'Данные клиента:\n{customer_data}\nТовары:\n{item_data}')
+        return update.message.reply_text(f'Данные клиента: {customer_data} \n Товары: {item_data}')
     except:
-        update.message.reply_text('Заказ не найден')
+        return update.message.reply_text('Заказ не найден')
 
 """commands"""
 def start(update, context):
@@ -36,16 +69,16 @@ def help(update, context):
     #sendtrack - email to customer with track num
     #lastorders - orders for last week from DB
     commands = ['/start', '/help', '/orderinfo', '/sendtrack', '/lastorders']
-    update.message.reply_text(f'Доступные команды: \n {commands}')
+    service_commands = ['/timer', '/db_count']
+    update.message.reply_text(f'Доступные команды: \n {commands} \n Служебные команды: \n {service_commands}')
 
 def orderinfo(update, context):
-    #update.message.reply_text('Введи номер заказа')
     order_id = int(update.message.text)
     get_order_info(update, order_id)
     
     
-"""destribute user's text to func"""
-def distributor(update, context):
+"""distribute user's text to func"""
+def distributor(update, context): #has a conflict between this func and others
     update.message.reply_text('Введи номер заказа')
     text_received = update.message.text
     if text_received.isnumeric():
@@ -53,17 +86,20 @@ def distributor(update, context):
 
 """main func"""
 def main():
-    TOKEN = " "
+    TOKEN = "5303733211:AAGWjltJu7TnjA1DonWMp18KYyZSDDq0i0c"
     #create updater
     updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     #handlers for base commands
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('help', help))
+    dispatcher.add_handler(CommandHandler('timer', timer))
     #handlers for custom commands
     dispatcher.add_handler(CommandHandler('orderinfo', distributor))
     #text handler
     dispatcher.add_handler(MessageHandler(Filters.text, distributor))
+    #TEST HANDLERS
+    #dispatcher.add_handler(CommandHandler('db_count', db_count)) #execute new orders
     #activate updates
     updater.start_polling()
     #run
